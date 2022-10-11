29eb368ec44861317a8d1f883eab15247dfb121c
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-11 13:47:40 +0500

    ✨

---
+++ Thermo/Thermo/ThermoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermoApp.swift
+//  Thermo
+//
+//  Created by Safar Safarov on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ThermoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
