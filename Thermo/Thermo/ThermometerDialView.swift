41bf70cb9bd232398785031a3052dc1a114d00ff
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-15 15:18:46 +0500

    ✨

---
+++ Thermo/Thermo/ThermometerDialView.swift
@@ -0,0 +1,27 @@
+//
+//  ThermometerDialView.swift
+//  Thermo
+//
+//  Created by Safar Safarov on 16/10/22.
+//
+
+import SwiftUI
+
+struct ThermometerDialView: View {
+    private let outerDialSize: CGFloat = 200
+    private let innerDialSize: CGFloat = 172
+    private let setPointSize: CGFloat = 15
+    var degrees: CGFloat = 0
+    
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+struct ThermometerDialView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThermometerDialView()
+    }
+}
