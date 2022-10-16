e392cbc45f48cd4ff90aeb71208ffc47ae38b4fc
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-16 15:37:52 +0500

    ✨

--- Thermo/Thermo/ThermometerDialView.swift
+++ Thermo/Thermo/ThermometerDialView.swift
@@ -15,7 +15,12 @@ struct ThermometerDialView: View {
     
     var body: some View {
         ZStack{
-            
+            // MARK: Outer Dial
+            Circle()
+                .fill(LinearGradient([Color("Outer Dial 1"), Color("Outer Dial 2")]))
+                .frame(width: outerDialSize, height: outerDialSize)
+                .shadow(color: .black.opacity(0.2), radius: 60, x: 0, y: 30) // Drop Shadow 1
+                .shadow(color: .black.opacity(0.2), radius: 16, x: 0, y: 8)
         }
     }
 }
@@ -23,5 +28,7 @@ struct ThermometerDialView: View {
 struct ThermometerDialView_Previews: PreviewProvider {
     static var previews: some View {
         ThermometerDialView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("Background"))
     }
 }
