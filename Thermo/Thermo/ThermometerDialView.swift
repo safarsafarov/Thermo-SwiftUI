10c70abe1667f643018412177f982d0034fa48b1
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-18 11:27:01 +0500

    ✨

--- Thermo/Thermo/ThermometerDialView.swift
+++ Thermo/Thermo/ThermometerDialView.swift
@@ -20,7 +20,34 @@ struct ThermometerDialView: View {
                 .fill(LinearGradient([Color("Outer Dial 1"), Color("Outer Dial 2")]))
                 .frame(width: outerDialSize, height: outerDialSize)
                 .shadow(color: .black.opacity(0.2), radius: 60, x: 0, y: 30) // Drop Shadow 1
-                .shadow(color: .black.opacity(0.2), radius: 16, x: 0, y: 8)
+                .shadow(color: .black.opacity(0.2), radius: 16, x: 0, y: 8) // Drop Shadow 2
+            
+                .overlay {
+                    // MARK: Outer Dial Border
+                    Circle()
+                        .stroke(LinearGradient([.white.opacity(0.2),
+                                                .black.opacity(0.19)]), lineWidth: 1)
+                }
+                .overlay {
+                    // MARK: Outer Dial Inner Shadow
+                    Circle()
+                        .stroke(.white.opacity(0.1), lineWidth: 4)
+                        .blur(radius: 8)
+                        .offset(x: 3, y: 3)
+                        .mask {
+                            Circle()
+                                .fill(LinearGradient([.black, .clear]))
+                        }
+                }
+            
+            // MARK: Inner Dial
+            Circle()
+                .fill(LinearGradient([Color("Inner Dial 1"), Color("Inner Dial 2")]))
+                .frame(width: innerDialSize, height: innerDialSize)
+            
+            // MARK: Temperature Setpoint
+            Circle()
+            
         }
     }
 }
