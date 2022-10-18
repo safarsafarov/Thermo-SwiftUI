852d37aacdacbf23bc6bb46348129bbf639fd3ac
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-18 15:18:28 +0500

    ✨

---
+++ Thermo/Thermo/SmartHomeThermostat.swift
@@ -0,0 +1,44 @@
+//
+//  SmartHomeThermostat.swift
+//  Thermo
+//
+//  Created by Safar Safarov on 18/10/22.
+//
+
+import SwiftUI
+
+struct SmartHomeThermostat: View {
+    var status: Status
+    var showStatus: Bool
+    var temperature: CGFloat
+    
+    
+    var body: some View {
+        VStack(spacing: 0){
+            // MARK: Temperature Status
+            Text(status.rawValue)
+                .font(.headline)
+                .foregroundColor(.white)
+                .opacity(showStatus ? 0.6 : 0)
+                .animation(.easeIn(duration: 0.5), value: showStatus)
+            
+            // MARK: Temperature
+            Text("\(temperature, specifier: "%.0f")")
+                .font(.system(size: 54))
+                .foregroundColor(.white)
+            
+            // MARK: Eco Leaf
+            Image(systemName: "leaf.fill")
+                .font(.title2.bold())
+                .foregroundColor(.green)
+        }
+        .padding(.top, 40)
+    }
+}
+
+struct SmartHomeThermostat_Previews: PreviewProvider {
+    static var previews: some View {
+        SmartHomeThermostat(status: .heating, showStatus: true, temperature: 22)
+            .background(Color("Inner Dial 2"))
+    }
+}
---
+++ Thermo/Thermo/ThermometerScaleView.swift
@@ -0,0 +1,62 @@
+//
+//  ThermometerScaleView.swift
+//  Thermo
+//
+//  Created by Safar Safarov on 18/10/22.
+//
+
+import SwiftUI
+
+struct ThermometerScaleView: View {
+    private let scaleSize: CGFloat = 276
+    private let horizontalPadding: CGFloat = 20
+    private let verticalPadding: CGFloat
+    
+    var body: some View {
+        ZStack {
+            // MARK: Scale Lines
+            ZStack {
+                ForEach(0..<21) { line in
+                    scaleLine(at: line)
+                }
+                .frame(width: scaleSize, height: scaleSize)
+                
+                // MARk: Temperature Markings
+                ZStack {
+                    HStack {
+                        Text("10°")
+                        Spacer()
+                        Text("30°")
+                    }
+                    
+                    VStack {
+                        Text("20°")
+                        Spacer()
+                    }
+                }
+                .frame(width: 390 - 2 * horizontalPadding, height: 390 - 2 * verticalPadding)
+                .font(.subheadline)
+                .foregroundColor(.white.opacity(0.3))
+            }
+        }
+    }
+    
+    func scaleLine(at line: Int) -> some View {
+        VStack {
+            Rectangle()
+                .fill(Color("Scale Line"))
+                .frame(width: 2, height: 10)
+            
+            Spacer()
+        }
+        .rotationEffect(.degrees(Double(line) * 9 - 90))
+    }
+}
+
+struct ThermometerScaleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThermometerScaleView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("Background"))
+    }
+}
